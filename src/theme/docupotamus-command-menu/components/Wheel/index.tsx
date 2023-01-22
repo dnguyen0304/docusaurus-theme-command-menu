05865f938e9fac79e759f785a456a7f709464a2a
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-22 11:14:53 -0700

    Add card area for Wheel component.

--- src/theme/docupotamus-command-menu/components/Wheel/index.tsx
+++ src/theme/docupotamus-command-menu/components/Wheel/index.tsx
@@ -1,6 +1,17 @@
 import Box from '@mui/material/Box';
-import { SxProps, Theme } from '@mui/material/styles';
+import { styled, SxProps, Theme } from '@mui/material/styles';
 import * as React from 'react';
+
+const StyledBox = styled(Box)({
+    // TODO(dnguyen0304): Fix missing responsive design.
+    width: '15%',
+    minWidth: '100px',
+    aspectRatio: '2.5 / 3.5',
+    placeSelf: 'center',
+
+    // TODO(dnguyen0304): Remove development code.
+    backgroundColor: 'red',
+});
 
 interface Props {
     readonly sx?: SxProps<Theme>;
@@ -8,8 +19,8 @@ interface Props {
 
 export default function Wheel({ sx }: Props): JSX.Element {
     return (
-        <Box sx={{ ...sx }}>
+        <StyledBox sx={{ ...sx }}>
             Wheel
-        </Box>
+        </StyledBox>
     );
 };

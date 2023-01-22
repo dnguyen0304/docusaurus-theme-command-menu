dde9ac0a9fb1971abd850382ed8905c23816f0bd
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-22 12:58:50 -0700

    Add translate for Slot styles.

--- src/theme/docupotamus-command-menu/components/Wheel/Slot/index.tsx
+++ src/theme/docupotamus-command-menu/components/Wheel/Slot/index.tsx
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
+import { styled, SxProps, Theme } from '@mui/material/styles';
 import * as React from 'react';
 
 const StyledBox = styled(Box)({
@@ -7,16 +7,31 @@ const StyledBox = styled(Box)({
     height: '100%',
 
     position: 'absolute',
+
+    // TODO(dnguyen0304): Remove development code.
+    outline: '2px solid red',
+
+    // backgroundColor: 'rgba(255, 255, 255, 0.01)',
+    // backdropFilter: 'blur(40px)',
+    // backgroundClip: 'padding-box',
+    // border: '2px solid transparent',
+    // boxShadow: '10px 10px 10px rgba(46, 54, 68, 0.03)',
 });
 
 interface Props {
     readonly index: number;
+    readonly sx?: SxProps<Theme>;
 };
 
-export default function Slot({ index }: Props): JSX.Element {
+export default function Slot(
+    {
+        index,
+        sx,
+    }: Props,
+): JSX.Element {
     return (
-        <StyledBox>
-            Slot
+        <StyledBox sx={{ ...sx }}>
+            {`Slot ${index}`}
         </StyledBox>
     );
 };
--- src/theme/docupotamus-command-menu/components/Wheel/index.tsx
+++ src/theme/docupotamus-command-menu/components/Wheel/index.tsx
@@ -7,7 +7,7 @@ const StyledBox = styled(Box)({
     // TODO(dnguyen0304): Fix missing responsive design.
     width: '15%',
     minWidth: '100px',
-    aspectRatio: '2.5 / 3.5',
+    aspectRatio: '2.5 / 3',
 
     position: 'relative',
     placeSelf: 'center',
@@ -21,17 +21,20 @@ interface Props {
 };
 
 export default function Wheel({ sx }: Props): JSX.Element {
+    // TODO(dnguyen0304): Fix missing responsive design.
+    // TODO(dnguyen0304): Investigate changing to use vmax or vmin so width and
+    //   height are relative to the same unit.
     const slots = [
-        {},
-        {},
-        {},
-        {},
-        {},
+        { sx: { translate: '0 -100%' } },
+        { sx: { translate: '150% -30%' } },
+        { sx: { translate: '80% 100%' } },
+        { sx: { translate: '-80% 100%' } },
+        { sx: { translate: '-150% -30%' } },
     ];
 
     return (
         <StyledBox sx={{ ...sx }}>
-            {slots.map((slot, index) => <Slot index={index} />)}
+            {slots.map((slot, index) => <Slot index={index} sx={slot.sx} />)}
         </StyledBox>
     );
 };

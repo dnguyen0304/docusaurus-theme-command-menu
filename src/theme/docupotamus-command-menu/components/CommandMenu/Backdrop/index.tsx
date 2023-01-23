dc68c017006cde409862a99856156bf20c9e4037
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-23 10:40:35 -0700

    Add Tiles for CommandMenu/Backdrop component.

--- src/theme/docupotamus-command-menu/components/CommandMenu/Backdrop/index.tsx
+++ src/theme/docupotamus-command-menu/components/CommandMenu/Backdrop/index.tsx
@@ -5,6 +5,9 @@ import { styled } from '@mui/material/styles';
 import { keyframes } from '@mui/system';
 import * as React from 'react';
 
+const TILE_SIZE_PX: number = 100;
+const TILE_BORDER_WIDTH_PX: number = 0.5;
+
 const getAnimation = (): Keyframes => {
     return keyframes({
         from: {
@@ -29,18 +32,65 @@ const Gradient = styled(Box)({
     backgroundSize: '200%',
     overflow: 'hidden',
 
-    animationDuration: '5s',
+    animationDuration: '3s',
     animationIterationCount: 'infinite',
     animationName: getAnimation(),
     animationTimingFunction: 'linear',
 });
 
+const TilesLayout = styled(Box)({
+    position: 'absolute',
+    width: `calc(100vw - ${TILE_BORDER_WIDTH_PX}px)`,
+    height: `calc(100vh - ${TILE_BORDER_WIDTH_PX}px)`,
+
+    display: 'grid',
+    gridTemplateColumns: `repeat(auto-fit, minmax(${TILE_SIZE_PX}px, 1fr))`,
+    gridTemplateRows: `repeat(auto-fit, minmax(${TILE_SIZE_PX}px, 1fr))`,
+});
+
+const Tile = styled(Box)({
+    position: 'relative',
+    '&:before': {
+        content: '""',
+        position: 'absolute',
+        backgroundColor: 'rgb(15, 15, 15)',
+        inset: `${TILE_BORDER_WIDTH_PX}px`,
+    },
+});
+
+const getViewportWidth = (): number => {
+    return window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+};
+
+const getViewportHeight = (): number => {
+    return window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+};
+
 interface Props extends MuiBackdropProps { };
 
 export default function Backdrop(props: Props): JSX.Element {
+    const [tileCount, setTileCount] = React.useState<number>(0);
+
+    const tilesRef = React.useRef();
+
+    React.useEffect(() => {
+        const columnCount = Math.floor(getViewportWidth() / TILE_SIZE_PX);
+        const rowCount = Math.floor(getViewportHeight() / TILE_SIZE_PX);
+        setTileCount(columnCount * rowCount);
+    }, []);
+
     return (
         <MuiBackdrop {...props}>
             <Gradient />
+            <TilesLayout ref={tilesRef}>
+                {[...Array(tileCount)].map((_, i) =>
+                    <Tile key={`tile-${i}`} />
+                )}
+            </TilesLayout>
         </MuiBackdrop>
     );
 };

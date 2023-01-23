4a0e0cd29d045933295ec95e94359f16d190586a
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-23 11:12:54 -0700

    Add useDomRect hook.

---
+++ src/theme/docupotamus-command-menu/hooks/useDomRect/index.tsx
@@ -0,0 +1,16 @@
+import useResizeObserver from '@react-hook/resize-observer';
+import * as React from 'react';
+
+export default function useDomRect<T extends HTMLElement>(
+    target: React.MutableRefObject<T>,
+): DOMRect | undefined {
+    const [domRect, setDomRect] = React.useState<DOMRect>();
+
+    React.useLayoutEffect(() => {
+        setDomRect(target.current?.getBoundingClientRect())
+    }, [target]);
+
+    useResizeObserver(target, (entry) => setDomRect(entry.contentRect));
+
+    return domRect;
+};

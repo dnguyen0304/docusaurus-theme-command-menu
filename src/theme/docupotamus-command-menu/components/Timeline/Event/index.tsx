3dbe1a0f2be7ff182b24048cf81eff1981387d5b
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-24 09:27:17 -0700

    Add dot anchor for Timeline/Event component.

--- src/theme/docupotamus-command-menu/components/Timeline/Event/index.tsx
+++ src/theme/docupotamus-command-menu/components/Timeline/Event/index.tsx
@@ -13,11 +13,19 @@ export default function Event(
     }: Props,
 ): JSX.Element {
     return (
-        <Box component='section'>
-            <h2>{heading}</h2>
-            <p>{snippet}</p>
-            <Box>{type}</Box>
-            <Box>{timestampMilli}</Box>
+        <Box>
+            <Box sx={{
+                width: '10px',
+                height: '10px',
+                border: '2px solid blue',
+                borderRadius: '50%',
+            }} />
+            <Box component='section'>
+                <h2>{heading}</h2>
+                <p>{snippet}</p>
+                <Box>{type}</Box>
+                <Box>{timestampMilli}</Box>
+            </Box>
         </Box>
     );
 };

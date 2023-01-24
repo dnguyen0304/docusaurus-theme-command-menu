c96ab04b7bdbab46791723cb23590d59f54b0317
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-24 13:01:50 -0700

    Add card styles for Timeline/Event styles.

--- src/theme/docupotamus-command-menu/components/Timeline/Event/index.tsx
+++ src/theme/docupotamus-command-menu/components/Timeline/Event/index.tsx
@@ -1,6 +1,22 @@
 import { TimelineEventData } from '@docusaurus/theme-command-menu';
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
+
+const StyledCard = styled(Box)({
+    width: '80%',
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+
+    borderRadius: 'var(--space-m)',
+    color: 'var(--docupotamus-color-grey-800)',
+    fontFamily: 'var(--docupotamus-font-family)',
+    fontSize: 'var(--docupotamus-font-size)',
+    fontWeight: 'var(--docupotamus-body-font-weight)',
+    lineHeight: '1.2',
+});
 
 interface Props extends TimelineEventData { };
 
@@ -13,19 +29,19 @@ export default function Event(
     }: Props,
 ): JSX.Element {
     return (
-        <Box>
+        <Box sx={{ width: '100%' }}>
             <Box sx={{
                 width: '10px',
                 height: '10px',
                 border: '2px solid blue',
                 borderRadius: '50%',
             }} />
-            <Box component='section'>
+            <StyledCard component='section'>
                 <h2>{heading}</h2>
                 <p>{snippet}</p>
                 <Box>{type}</Box>
                 <Box>{timestampMilli}</Box>
-            </Box>
+            </StyledCard>
         </Box>
     );
 };

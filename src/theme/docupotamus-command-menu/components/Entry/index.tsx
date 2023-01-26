6617dec765bc115e20dd2955fb33ad4957222537
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-26 15:20:10 -0700

    Add ClippingBox for Entry component.

--- src/theme/docupotamus-command-menu/components/Entry/index.tsx
+++ src/theme/docupotamus-command-menu/components/Entry/index.tsx
@@ -1,11 +1,22 @@
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 import * as React from 'react';
 import Gradient from '../common/Gradient';
 
+const ClippingBox = styled(Box)({
+    width: '100px',
+    height: '100px',
+
+    position: 'fixed',
+    bottom: 0,
+
+    backgroundColor: 'red',
+});
+
 export default function Entry(): JSX.Element {
     return (
-        <Box sx={{ position: 'fixed', bottom: 0, width: '100px', height: '100px', backgroundColor: 'red' }} >
+        <ClippingBox>
             <Gradient />
-        </Box>
+        </ClippingBox>
     );
 };

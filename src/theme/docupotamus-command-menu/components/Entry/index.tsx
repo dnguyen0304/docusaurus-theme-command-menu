27cd8e9414f8823e11d0a771da037e3b8db27b88
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-26 15:18:09 -0700

    Add Gradient for Entry component.

--- src/theme/docupotamus-command-menu/components/Entry/index.tsx
+++ src/theme/docupotamus-command-menu/components/Entry/index.tsx
@@ -1,10 +1,11 @@
 import Box from '@mui/material/Box';
 import * as React from 'react';
+import Gradient from '../common/Gradient';
 
 export default function Entry(): JSX.Element {
     return (
         <Box sx={{ position: 'fixed', bottom: 0, width: '100px', height: '100px', backgroundColor: 'red' }} >
-            no
+            <Gradient />
         </Box>
     );
-}
+};

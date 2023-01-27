4e1ccb4a40c6b9837f16898937f929b2c13ff01b
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-27 04:49:57 -0700

    Add Annotater/Tooltip component.

---
+++ src/theme/docupotamus-command-menu/components/Annotater/Tooltip/index.tsx
@@ -0,0 +1,33 @@
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import StarButton from './StarButton';
+import styles from './styles.module.css';
+
+interface Props {
+    readonly isVisible: boolean;
+    readonly positionTopPx: number;
+    readonly positionLeftPx: number;
+};
+
+export default function Tooltip(
+    {
+        isVisible,
+        positionTopPx,
+        positionLeftPx,
+    }: Props,
+): JSX.Element {
+    return (
+        <Box
+            className={styles.Tooltip}
+            sx={{
+                top: `${positionTopPx}px`,
+                left: `${positionLeftPx}px`,
+                visibility: isVisible ? 'visible' : 'hidden',
+            }}
+        // onMouseDown={e => e.preventDefault()}
+        // role='presentation'
+        >
+            <StarButton onClick={() => console.log('clicked star button')} />
+        </Box>
+    );
+};

add8fa6af957d828e3b3c90a8d6b261b23c50845
Duy Nguyen <dnguyen0304@gmail.com>
2023-01-27 11:48:50 -0700

    Fix missing readonly for DocSidebar/Desktop/Content component.

--- src/theme/docupotamus-command-menu/DocSidebar/Desktop/Content/index.tsx
+++ src/theme/docupotamus-command-menu/DocSidebar/Desktop/Content/index.tsx
@@ -4,7 +4,7 @@ import type ContentType from '@theme/DocSidebar/Desktop/Content';
 import * as React from 'react';
 import Entry from '../../../components/Entry';
 
-type Props = WrapperProps<typeof ContentType>;
+type Props = Readonly<WrapperProps<typeof ContentType>>;
 
 export default function ContentWrapper(props: Props): JSX.Element {
     return (

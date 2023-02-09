91e2437e2cae6f5922f755aa20081115e0330dc3
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-09 15:44:18 -0700

    Add package directory.

---
+++ src/theme/docupotamus-command-menu/package/index.tsx
@@ -0,0 +1,7 @@
+// Warning: Only symbols exported from this package/ directory are considered
+// public. Other symbols are considered private and _unsafe_ to be imported.
+// Their APIs are unstable and breaking changes might occur.
+
+export { default as DocItemContent } from '../swizzle/DocItem/Content';
+export { default as DocSidebarDesktopContent } from '../swizzle/DocSidebar/Desktop/Content';
+export { default as Root } from '../swizzle/Root';

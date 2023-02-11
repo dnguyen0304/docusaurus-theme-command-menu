1c1e9254dbc82aa5281f63f0464dfbaea7d7ee62
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-11 07:49:15 -0700

    Fix broken references for swizzle directory.

--- src/theme/swizzle/DocItem/Content/index.tsx
+++ src/theme/swizzle/DocItem/Content/index.tsx
@@ -2,7 +2,7 @@ import type { WrapperProps } from '@docusaurus/types';
 import Content from '@theme-init/DocItem/Content';
 import type ContentType from '@theme/DocItem/Content';
 import * as React from 'react';
-import Annotater from '../../../docupotamus-command-menu/components/Annotater';
+import Annotater from '../../../../package/components/Annotater';
 
 type Props = Readonly<WrapperProps<typeof ContentType>>;
 
--- src/theme/swizzle/Root/index.tsx
+++ src/theme/swizzle/Root/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import CommandMenu from '../../docupotamus-command-menu/components/CommandMenu';
-import { AddressBarProvider } from '../../docupotamus-command-menu/contexts/address-bar';
-import { CommandMenuProvider } from '../../docupotamus-command-menu/contexts/command-menu';
-import { TimelineProvider } from '../../docupotamus-command-menu/contexts/timeline';
-import { WheelProvider } from '../../docupotamus-command-menu/contexts/wheel';
-import CommandMenuKeyboardShortcuts from '../../docupotamus-command-menu/keyboard-shortcuts/CommandMenu';
-import '../../docupotamus-command-menu/styles.css';
+import CommandMenu from '../../../package/components/CommandMenu';
+import { AddressBarProvider } from '../../../package/contexts/address-bar';
+import { CommandMenuProvider } from '../../../package/contexts/command-menu';
+import { TimelineProvider } from '../../../package/contexts/timeline';
+import { WheelProvider } from '../../../package/contexts/wheel';
+import CommandMenuKeyboardShortcuts from '../../../package/keyboard-shortcuts/CommandMenu';
+import '../../../package/styles.css';
 
 interface Props {
     readonly children: React.ReactNode;

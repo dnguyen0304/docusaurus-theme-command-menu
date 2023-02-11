5c6717f4ef31517f8744db9c0b4295455170af5a
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-11 16:45:00 -0700

    Add public.

--- src/public/docupotamus-command-menu.ts
+++ src/public/docupotamus-command-menu.ts
@@ -1,7 +1,3 @@
-// Warning: Only symbols exported from this package/ directory are considered
-// public. Other symbols are considered private and _unsafe_ to be imported.
-// Their APIs are unstable and breaking changes might occur.
-
-// export { default as DocItemContent } from '../swizzle/DocItem/Content';
-// export { default as DocSidebarDesktopContent } from '../swizzle/DocSidebar/Desktop/Content';
-// export { default as Root } from '../swizzle/Root';
+export { default as DocItemContent } from '../theme/decorators/DocItem/Content';
+export { default as DocSidebarDesktopContent } from '../theme/decorators/DocSidebar/Desktop/Content';
+export { default as Root } from '../theme/decorators/Root';

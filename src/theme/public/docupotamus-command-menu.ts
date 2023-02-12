8e53f8dea1d76c6e0fb3b1e3081ee2750cf1bee1
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-11 17:00:10 -0700

    Rename exports to *Decorator for public directory.

--- src/theme/public/docupotamus-command-menu.ts
+++ src/theme/public/docupotamus-command-menu.ts
@@ -1,3 +1,3 @@
-export { default as DocItemContent } from '../decorators/DocItem/Content';
-export { default as DocSidebarDesktopContent } from '../decorators/DocSidebar/Desktop/Content';
-export { default as Root } from '../decorators/Root';
+export { default as DocItemContentDecorator } from '../decorators/DocItem/Content';
+export { default as DocSidebarDesktopContentDecorator } from '../decorators/DocSidebar/Desktop/Content';
+export { default as RootDecorator } from '../decorators/Root';

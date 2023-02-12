c6f4d048e3eae9a05e9e337cc18a13aab1f52e4b
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-11 17:17:22 -0700

    Remove duplicated for for DocItem/Content swizzle.

--- src/theme/swizzle/DocItem/Content/index.tsx
+++ src/theme/swizzle/DocItem/Content/index.tsx
@@ -2,15 +2,14 @@ import type { WrapperProps } from '@docusaurus/types';
 import Content from '@theme-init/DocItem/Content';
 import type ContentType from '@theme/DocItem/Content';
 import * as React from 'react';
-import Annotater from '../../../package/components/Annotater';
+import Decorator from '../../../decorators/DocItem/Content';
 
 type Props = Readonly<WrapperProps<typeof ContentType>>;
 
 export default function ContentWrapper(props: Props): JSX.Element {
     return (
-        <>
+        <Decorator>
             <Content {...props} />
-            <Annotater />
-        </>
+        </Decorator>
     );
 };

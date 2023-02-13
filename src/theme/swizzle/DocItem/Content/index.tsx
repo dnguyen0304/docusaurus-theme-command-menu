0c9d0c777b488ed493ac0f335b868039dd0be137
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-13 09:32:12 -0700

    Rename Content alias to ContentInit for swizzle.

--- src/theme/swizzle/DocItem/Content/index.tsx
+++ src/theme/swizzle/DocItem/Content/index.tsx
@@ -1,5 +1,5 @@
 import type { WrapperProps } from '@docusaurus/types';
-import Content from '@theme-init/DocItem/Content';
+import ContentInit from '@theme-init/DocItem/Content';
 import type ContentType from '@theme/DocItem/Content';
 import * as React from 'react';
 import Decorator from '../../../decorators/DocItem/Content';
@@ -9,7 +9,7 @@ type Props = Readonly<WrapperProps<typeof ContentType>>;
 export default function ContentWrapper(props: Props): JSX.Element {
     return (
         <Decorator>
-            <Content {...props} />
+            <ContentInit {...props} />
         </Decorator>
     );
 };
--- src/theme/swizzle/DocSidebar/Desktop/Content/index.tsx
+++ src/theme/swizzle/DocSidebar/Desktop/Content/index.tsx
@@ -1,5 +1,5 @@
 import type { WrapperProps } from '@docusaurus/types';
-import Content from '@theme-init/DocSidebar/Desktop/Content';
+import ContentInit from '@theme-init/DocSidebar/Desktop/Content';
 import type ContentType from '@theme/DocSidebar/Desktop/Content';
 import * as React from 'react';
 import Decorator from '../../../../decorators/DocSidebar/Desktop/Content';
@@ -9,7 +9,7 @@ type Props = Readonly<WrapperProps<typeof ContentType>>;
 export default function ContentWrapper(props: Props): JSX.Element {
     return (
         <Decorator>
-            <Content {...props} />
+            <ContentInit {...props} />
         </Decorator>
     );
 };

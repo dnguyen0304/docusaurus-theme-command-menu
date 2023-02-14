3a551b356ec1b7c608fd62086f62349f256e9992
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-14 12:19:35 -0700

    Migrate to d9s custom property namespace for styles.

--- src/theme/package/constants.ts
+++ src/theme/package/constants.ts
@@ -1,2 +1,3 @@
 export const TILE_BORDER_WIDTH_PX: number = 0.5;
-export const FOOTER_HEIGHT: React.CSSProperties['height'] = 'var(--space-l)';
+export const FOOTER_HEIGHT: React.CSSProperties['height'] =
+    'var(--d9s-space-l)';

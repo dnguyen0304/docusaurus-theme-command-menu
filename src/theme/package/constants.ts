b4ad9b5c031da5ab6cdc021a6e82a46dde1bebc9
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-16 13:04:17 -0700

    Increase TILE_BORDER_WIDTH_PX for constants.

--- src/theme/package/constants.ts
+++ src/theme/package/constants.ts
@@ -1,5 +1,5 @@
 // Warning: This is deprecated. Instead use CSS custom properties.
 
-export const TILE_BORDER_WIDTH_PX: number = 0.5;
+export const TILE_BORDER_WIDTH_PX: number = 1;
 export const FOOTER_HEIGHT: React.CSSProperties['height'] =
     'var(--d9s-space-l)';

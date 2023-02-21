d6a79b70c1640f768abd0c82962e3773ff6332dd
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-21 14:54:10 -0700

    Add flicker animation for DocItem/Content decorator.

--- src/theme/decorators/DocItem/Content/index.tsx
+++ src/theme/decorators/DocItem/Content/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Annotater from '../../../package/components/Annotater';
 import { SEARCH_PARAM_SELECTOR_ENCODED } from '../../../package/constants';
 import { SelectionProvider } from '../../../package/contexts/selection';
+import styles from './styles.module.css';
 
 const scrollToRange = () => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -17,10 +18,13 @@ const scrollToRange = () => {
             .toRange();
     // TODO(dnguyen0304): Investigate different approach.
     //   See: http://roysharon.com/blog/37
-    range.startContainer.parentElement?.scrollIntoView({
+    const startElement = range.startContainer.parentElement
+    startElement?.scrollIntoView({
         behavior: 'smooth',
         block: 'center',
     });
+    startElement?.classList?.remove(styles.target__flicker ?? '');
+    startElement?.classList?.add(styles.target__flicker ?? '');
 };
 
 interface Props {

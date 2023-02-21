15f4a219f310f09e5136e1907406da6ba2157bd1
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-21 14:34:52 -0700

    Add scrollToRange on load for DocItem/Content decorator.

--- src/theme/decorators/DocItem/Content/index.tsx
+++ src/theme/decorators/DocItem/Content/index.tsx
@@ -1,12 +1,42 @@
+import { RangeAnchor } from '@docupotamus/docusaurus-lib-common/annotate/anchor';
 import * as React from 'react';
 import Annotater from '../../../package/components/Annotater';
 import { SelectionProvider } from '../../../package/contexts/selection';
+
+const scrollToRange = () => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const selectorEncoded = searchParams.get('selectorEncoded');
+    if (!selectorEncoded) {
+        return;
+    }
+    const selector = JSON.parse(atob(selectorEncoded));
+    const range =
+        RangeAnchor
+            .fromSelector(document.body, selector)
+            .toRange();
+    range.startContainer.parentElement?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+    });
+};
 
 interface Props {
     readonly children: React.ReactNode;
 };
 
 export default function ContentDecorator({ children }: Props): JSX.Element {
+    React.useEffect(() => {
+        // TODO(dnguyen0304): Investigate the differences between
+        //   DOMContentLoaded, load, and readystatechange.
+        if (document.readyState === 'complete') {
+            scrollToRange();
+            return;
+        } else {
+            window.addEventListener('load', scrollToRange);
+            return () => window.removeEventListener('load', scrollToRange);
+        }
+    }, []);
+
     return (
         <SelectionProvider>
             {children}

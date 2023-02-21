dc9a92c5c2fdc9e587111255d082b87572cdbcdb
Duy Nguyen <dnguyen0304@gmail.com>
2023-02-20 17:03:26 -0700

    Move useEffect to ButtonGroup for Annotater component.

--- src/theme/package/components/Annotater/ButtonGroup/index.tsx
+++ src/theme/package/components/Annotater/ButtonGroup/index.tsx
@@ -1,6 +1,9 @@
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import * as React from 'react';
+import { useSelection } from '../../../contexts/selection';
+import { SelectionTopCenterPositioner } from '../../../services/annotate/tooltip/positioner';
+import * as rangeUtils from '../../../services/annotate/utils/range'; /* TODO(dnguyen0304): Fix missing type declaration. */
 import StarButton from './StarButton';
 
 const StyledBox = styled(Box)({
@@ -38,22 +41,43 @@ const StyledBox = styled(Box)({
 });
 
 interface Props {
-    readonly isVisible: boolean;
-    readonly positionTopPx: number;
-    readonly positionLeftPx: number;
     readonly starIsClicked: boolean;
     readonly starOnClick: () => void;
 };
 
 export default function ButtonGroup(
     {
-        isVisible,
-        positionTopPx,
-        positionLeftPx,
         starIsClicked,
         starOnClick,
     }: Props,
 ): JSX.Element {
+    const { range } = useSelection();
+
+    const [isVisible, setIsVisible] = React.useState<boolean>(false);
+    const [positionTopPx, setPositionTopPx] = React.useState<number>(0);
+    const [positionLeftPx, setPositionLeftPx] = React.useState<number>(0);
+
+    React.useEffect(() => {
+        if (range) {
+            const selection = document.getSelection()!;
+            const focusRect = rangeUtils.selectionFocusRect(selection);
+            if (!focusRect) {
+                // The selected range does not contain any text.
+                return;
+            }
+            const isBackwards = rangeUtils.isSelectionBackwards(selection);
+            const { left, top } = new SelectionTopCenterPositioner().position(
+                focusRect,
+                isBackwards,
+            );
+            setIsVisible(true);
+            setPositionTopPx(top);
+            setPositionLeftPx(left);
+        } else {
+            setIsVisible(false);
+        }
+    }, [range]);
+
     return (
         <StyledBox
             sx={{
--- src/theme/package/components/Annotater/index.tsx
+++ src/theme/package/components/Annotater/index.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import { useSelection } from '../../contexts/selection';
-import { SelectionTopCenterPositioner } from '../../services/annotate/tooltip/positioner';
-import * as rangeUtils from '../../services/annotate/utils/range'; /* TODO(dnguyen0304): Fix missing type declaration. */
 import ButtonGroup from './ButtonGroup';
 
 export default function Annotater(): JSX.Element {
-    const { range } = useSelection();
-
-    const [isVisible, setIsVisible] = React.useState<boolean>(false);
-    const [positionTopPx, setPositionTopPx] = React.useState<number>(0);
-    const [positionLeftPx, setPositionLeftPx] = React.useState<number>(0);
     const [starIsClicked, setStarIsClicked] = React.useState<boolean>(false);
 
     const starHandleClick = () => {
@@ -26,32 +18,8 @@ export default function Annotater(): JSX.Element {
         setStarIsClicked(prev => !prev);
     };
 
-    React.useEffect(() => {
-        if (range) {
-            const selection = document.getSelection()!;
-            const focusRect = rangeUtils.selectionFocusRect(selection);
-            if (!focusRect) {
-                // The selected range does not contain any text.
-                return;
-            }
-            const isBackwards = rangeUtils.isSelectionBackwards(selection);
-            const { left, top } = new SelectionTopCenterPositioner().position(
-                focusRect,
-                isBackwards,
-            );
-            setIsVisible(true);
-            setPositionTopPx(top);
-            setPositionLeftPx(left);
-        } else {
-            setIsVisible(false);
-        }
-    }, [range]);
-
     return (
         <ButtonGroup
-            isVisible={isVisible}
-            positionTopPx={positionTopPx}
-            positionLeftPx={positionLeftPx}
             starIsClicked={starIsClicked}
             starOnClick={starHandleClick}
         />
